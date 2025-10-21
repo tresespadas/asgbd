1b1d878dfe45231cf6a1b731625bed56f375c61b
tresespadas <alvaro8038c@gmail.com>
2025-10-21 13:44:18 +0200

    triggers corregidos

--- ud2/actividad1_triggers.sql
+++ ud2/actividad1_triggers.sql
@@ -9,28 +9,39 @@ DELIMITER ;
 DELIMITER $$
 CREATE TRIGGER trigger_acumulativo AFTER INSERT ON cuentas FOR EACH ROW
 BEGIN
-   SET @total = @total + NEW.saldo;
+   SET @saldo = @saldo + NEW.saldo;
 END; $$
 DELIMITER ;
 
+SET @saldo=0;
+
 INSERT INTO cuentas VALUES(1,150.39);
-SELECT @total;
+SELECT @saldo;
 
 DELIMITER $$
 CREATE TRIGGER trigger_resta BEFORE DELETE ON cuentas FOR EACH ROW
 BEGIN
-   SET @total = @total - OLD.saldo;
+   SET @saldo = @saldo - OLD.saldo;
 END; $$
 DELIMITER ;
 
 DELETE FROM cuentas WHERE num_cuenta=1;
-SELECT @total;
+SELECT @saldo;
 
 
 -- Ejercicio 2
 DELIMITER $$
+CREATE PROCEDURE pr_calculo_comision(importe DECIMAL)
+BEGIN
+   SET @var_global_comision = importe / 5;
+END;
+$$
+DELIMITER ;
+
+DELIMITER $$
 CREATE TRIGGER trigger_comision AFTER INSERT on ventas FOR EACH ROW
 BEGIN
-   SET @importecomision = NEW.importeventa * 1.2;
+   CALL pr_calculo_comision(NEW.importe);
+   SET NEW.importecomision = @var_global_comision;
 END; $$
 DELIMITER ;
